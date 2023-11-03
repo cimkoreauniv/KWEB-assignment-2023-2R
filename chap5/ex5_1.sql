4ed07f1986bfc51d4e830dee045624e1b7a55556
cimkoreauniv <changilmoon@korea.ac.kr>
2023-11-03 14:06:51 +0900

    assignment 5 template

---
+++ chap5/ex5_1.sql
@@ -0,0 +1,26 @@
+--1
+SELECT * FROM `crud`;
+
+--2
+SELECT * FROM `crud`;
+
+--3
+SELECT * FROM `crud`;
+
+--4
+SELECT * FROM `crud`;
+
+--5
+SELECT * FROM `crud`;
+
+--6
+SELECT * FROM `crud`;
+
+--7
+SELECT * FROM `crud`;
+
+--8
+SELECT * FROM `crud`;
+
+--9
+SELECT * FROM `crud`;
---
+++ chap5/ex5_2.sql
@@ -0,0 +1,17 @@
+--1
+SELECT * FROM `crud`;
+
+--2
+SELECT * FROM `crud`;
+
+--3
+SELECT * FROM `crud`;
+
+--4
+SELECT * FROM `crud`;
+
+--5
+SELECT * FROM `crud`;
+
+--6
+SELECT * FROM `crud`;
